19354419b4a767dd0405fb341c91d3ee6b65f327
Hantlowt <lhote.antoine@gmail.com>
2021-04-20 23:23:43 +0200

    update

--- ihdw/__init__.py
+++ ihdw/__init__.py
@@ -3,6 +3,69 @@ from ihdb import Ihdb
 from hashlib import sha512
 from uuid import uuid4
 import time
+import os
+
+def create_global_config():
+    g = db.create_node('Global_Config')
+    g.id = 'Global_Config'
+    g.save()
+    g['__key__'] = uuid4().hex
+    g['website_name'] = 'Your WebSite'
+    g['website_url'] = 'https://your-website.com'
+    return g
+
+def hash_password(password):
+    return sha512((password+global_config['__key__']).encode()).hexdigest()
+
+def register_superadmin():
+    account = db.create_node('Account')
+    account['name'] = 'root'
+    account['__password__'] = hash_password('root')
+    account['isSuperAdmin'] = True
+
+class Admin:
+    def __init__(self, client):
+        self.__session__ = client.session
+        account_id = self.__session__.get_data('account_id', None)
+        self.account = db.node(account_id, 'Account') if account_id is not None else None
+        self.error = ''
+        self.info = ''
+        self.config = global_config
+
+    def login(self, name, password):
+        time.sleep(3) #Simple protection against brute-force attack
+        password_hash = hash_password(password)
+        try:
+            self.account = [a for a in db.nodes('Account') if a['name'] == name and a['__password__'] == password_hash][0]
+            self.__session__.data['account_id'] = self.account.id
+            self.error = ''
+            self.info = ''
+        except Exception:
+            self.error = "Incorrect name or password"
+    
+    def logout(self):
+        self.account = None
+        self.__session__.data['account_id'] = None
+    
+    def register(self, name, password):
+        account = db.create_node('Account')
+        account['name'] = name
+        account['__password__'] = hash_password(password)
+        account['isSuperAdmin'] = False
+    
+    def update_profile(self, name, password=''):
+        if len(name) > 0:
+            self.account['name'] = name
+            if len(password) > 0:
+                self.account['__password__'] = hash_password(password)
+            self.info = 'Account updated.'
+        else:
+            self.error = 'Name cannot be null.'
+    
+    def update_config(self, name, url):
+        self.config['website_name'] = name
+        self.config['website_url'] = url
+        self.info = 'Config updated.'
 
 db = Ihdb('db')
 global_config = db.node('Global_Config', 'Global_Config')
@@ -10,44 +73,8 @@ if global_config is None:
     global_config = create_global_config()
     register_superadmin()
 
-def create_global_config():
-    g = db.create_node('Global_Config')
-    g.id = 'Global_Config'
-    g.save()
-    g['key'] = uuid4().hex
-    g['website_name'] = 'Your WebSite'
-    return g
-
-def hash_password(password):
-    return sha512((password+g['key']).encode()).hexdigest()
-
-def register_superadmin():
-    account = db.create_node('Account')
-    accont['name'] = 'root'
-    account['password'] = hash_password('root')
-    account['isSuperAdmin'] = True
-
-class Admin:
-    def __init__(self, client):
-        self.account_id = client.session.get_data('account_id', None)
-        self.account = db.node(self.account_id, 'Account') if self.account_id is not None else None
-        self.error = ''
-        self.website_name = g['website_name']
-
-    def login(name, password):
-        time.sleep(3) #Simple protection against brute-force attack
-        password_hash = hash_password(password)
-        try:
-            self.account = [a for a in db.nodes('Account') if a['name'] = name and a['password'] == password_hash][0]
-        except Exception:
-            print("Incorrect name or password")
-    
-    def register(name, password):
-        account = db.create_node('Account')
-        account['name'] = name
-        account['password'] = hash_password(password)
-        account['isSuperAdmin'] = False
-
-
 def serve(host='localhost', port=1910):
-    Ihf(Todo, 'admin.html', host=host, port=port).serve()
+    Ihf(Admin, os.path.dirname(os.path.realpath(__file__))+'/index.html', host=host, port=port).serve()
+
+if __name__ == "__main__":
+    serve()
